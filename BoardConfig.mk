ae3db54871cbab6e17405312761536b629e25181
ShivamKumarJha <jha.shivam3@gmail.com>
2019-11-04 07:31:02 +0000

    Linx_X1_3G-user 8.1.0 O11019 1539777552 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,8 @@
+DEVICE_PATH := device/digma/LS4050MG
+BOARD_VENDOR := digma
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
+-include vendor/digma/LS4050MG/BoardConfigVendor.mk
